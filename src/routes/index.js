2613decf0dbbe13254c4c4bfb0d1ab9201538104
princeorji <michaelorji8039@gmail.com>
2025-07-18 10:31:31 +0100

    add post routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,7 @@
 const userRouter = require('./user')
+const postRouter = require('./post')
 
 module.exports = (app) => {
     app.use('/api/v1', userRouter)
+    app.use('/api/v1/posts', postRouter)
 }
